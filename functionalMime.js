30717d5db8225de8683c0895239f9a8e835d5eba
JiayuZhai <zjypersonal@163.com>
2022-02-16 10:55:43 +0800

    Create functionalMime.js

---
+++ functionalMime.js
@@ -0,0 +1,27 @@
+export const PICTURE = [
+  'image/png',
+  'image/gif',
+  'image/jpeg'
+]
+export const OFFICE_FILE = [
+  // .doc     
+  'application/msword',
+  // .docx
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  //.xls
+  'application/vnd.ms-excel application/x-excel',
+  //.xlsx
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  //.ppt
+  'application/vnd.ms-powerpoint',
+  //.pptx
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  //.pdf
+  'application/pdf'
+]
+export const VIDEO = [
+  'video/mp4',
+  'video/x-flv',
+  'video/quicktime',
+  'video/x-msvideo',
+]
