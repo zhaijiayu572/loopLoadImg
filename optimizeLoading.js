8a37c0fb439a85bae075ce2c8efe94bc7d20861b
JiayuZhai <zjypersonal@163.com>
2022-02-23 15:37:33 +0800

    Create optimizeLoading.js

---
+++ optimizeLoading.js
@@ -0,0 +1,15 @@
+const timeout = ms =>
+  new Promise((_, reject) =>
+    setTimeout(() => reject(Symbol.for('timeout')), ms)
+  )
+const rq = (api, ms, reject) => (...args) => {
+  const request = api(...args);
+  return Promise.race([request, timeout(ms)]).catch(err => {
+    // 判断是否是超时触发的reject
+    if (Symbol.for('timeout') === err) {
+      reject(err);
+      return request;
+    }
+    throw err;
+  })
+}
